0230bbbaa4126990070639bde0551af7ff97dae5
Victor Williams <victor.williams@stu.cu.edu.ng>
2024-05-13 22:26:00 +0100

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
